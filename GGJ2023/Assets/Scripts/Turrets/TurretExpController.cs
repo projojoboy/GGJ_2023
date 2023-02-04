c1aa852883f173288e65615598ee744126b3e6e2
DESKTOP-OQPBCM1\Joey <joeykossen@hotmail.com>
2023-02-04 14:47:16 +0100

    Add Turret prefab Add Exp bar to turret Add Exp

--- GGJ2023/Assets/Scripts/Turrets/TurretExpController.cs
+++ GGJ2023/Assets/Scripts/Turrets/TurretExpController.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TurretExpController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private int expReq = 20;
+    [SerializeField]
+    private int expCur = 0;
+
+    [SerializeField]
+    private Image expBar = null;
+
+    private void AddExp(int exp)
     {
-        
+        expCur += exp;
+        UpdateUI();
+
+        if(expCur >= expReq) 
+        { 
+            // On Level Up
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateUI()
     {
-        
+        expBar.fillAmount = (float)(1f / expReq * expCur);
     }
 }
